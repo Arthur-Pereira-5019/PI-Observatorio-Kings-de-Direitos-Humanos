b94c610a3cfd4c0dc574f75562a64eeed4d9ebeb
Arthur <peneir20@gmail.com>
2025-08-06 12:02:23 -0300

    Adicionado o básico da verificação de usuário e endpoints para tal, além de adicionadas algumas dependências para utilizar banco de dados

--- src/main/java/com/kings/okdhvi/Startup.java
+++ src/main/java/com/kings/okdhvi/Startup.java
@@ -2,8 +2,9 @@ package com.kings.okdhvi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class Startup {
 
 	public static void main(String[] args) {
