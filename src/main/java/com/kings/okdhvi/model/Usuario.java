0280045c4296e088e5f6de0aa9975abe7be52073
Arthur Pereira <arthur.p31@aluno.ifsc.edu.br>
2025-08-08 08:26:07 -0300

    Adicionada dependências de JPÁ e model de Usuario

---
+++ src/main/java/com/kings/okdhvi/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.kings.okdhvi.model;
+
+public class Usuario {
+    /*create table usuario
+            (
+                    nome varchar(100) not null,
+    senha varchar(100),
+
+    requisitavel boolean,
+    idEstadoDaConta char(8) primary key,
+    nomeEstado boolean,
+    aplicavelPorModerador boolean,
+    podeModerar boolean,
+    podePostar boolean,
+    podeConcederTitulacoes boolean,
+    podeAlteraroSite boolean
+);*/
+
+}
