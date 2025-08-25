578788ee2db8c8b5e0166deb1b41fa7f45ffda53
Daniel Stamm Pessoa de Oliveira <dani.stamm.pessoa.oliveira@gmail.com>
2025-08-25 15:24:59 -0300

    fetch feito(?)

--- src/main/resources/static/telaLoginLogica.js
+++ src/main/resources/static/telaLoginLogica.js
@@ -1,6 +1,47 @@
 const hideButtons = document.querySelectorAll(".hideButton");
+const hideIcons = document.querySelectorAll(".hideIcon");
+inputEmailRegistro = document.getElementById("inputEmailRegistro");
+inputCpfRegistro = document.getElementById("inputCpfRegistro");
+inputTelefoneRegistro = document.getElementById("inputTelefoneRegistro");
+senhaInputRegistro = document.getElementById("senhaInputRegistro");
+inputDataNascRegistro = document.getElementById("inputDataNascRegistro")
+confSenhaInputRegistro = document.getElementById("confSenhaInputRegistro");
 
-      const hideIcons = document.querySelectorAll(".hideIcon");
+
+
+const url = 'https://localhost:8080/api/user'; 
+const newPost = {
+  title: 'dadosRegistro',
+  body: '',
+  email: inputEmailRegistro.value,
+  cpf : inputCpfRegistro.value,
+  telefone : inputTelefoneRegistro.value,
+  senha : senhaInputRegistro.value,
+  dataNasc : inputDataNascRegistro.value 
+};
+
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('sem reposta');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Post feito:', data);
+  })
+  .catch(error => {
+    console.error('erro no post:', error);
+  });
+
+
 
     hideIcons.forEach(icon => {
         icon.addEventListener("click", () => {
@@ -16,3 +57,6 @@ const hideButtons = document.querySelectorAll(".hideButton");
             }
         });
     });
+
+
+
