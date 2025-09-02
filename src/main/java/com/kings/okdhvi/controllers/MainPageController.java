507d2ea4140f9b4824eeb5af0daeeb6797f8f560
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-09-02 14:15:47 -0300

    Merge manual no html da tela principal

--- src/main/java/com/kings/okdhvi/controllers/MainPageController.java
+++ src/main/java/com/kings/okdhvi/controllers/MainPageController.java
@@ -10,7 +10,7 @@ public class MainPageController {
 
     @GetMapping("/")
     public String mainScreen() {
-        return "paginaInicial";
+        return "telaInicial";
     }
 
     @GetMapping("login")
