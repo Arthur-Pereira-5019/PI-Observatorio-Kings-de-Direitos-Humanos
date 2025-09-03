d3fedb98f8504023174f515fe910a5a3c11a02f2
NatVansuita <natanjoaovansuita@gmail.com>
2025-09-03 09:04:28 -0300

    logica arrumada

--- src/main/resources/static/include.js
+++ src/main/resources/static/include.js
@@ -1,24 +1,36 @@
-const iconButton = document.getElementById("iconButton");
 
-iconButton.addEventListener("click", function(){
-    window.location.href = "http://localhost:8080/login"
+async function carregarHTML(id, url, cssFile) {
+    const response = await fetch(url);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
 
-})
 
-function loadHTML(id, url, cssFile) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-            if(cssFile){
-                let link = document.createElement("link")
-                link.rel = "stylesheet";
-                link.href = cssFile;
-                document.head.appendChild(link);
-
-            }
-        });
+    if (cssFile) {
+        let link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = cssFile;
+        document.head.appendChild(link);
+    }
 }
 
-loadHTML("header", "/cabecalho", "rodapeStyle.css");
-loadHTML("footer", "/rodape", "cabecalhoStyle.css");
+
+
+async function iniciar() {
+    await carregarHTML("header", "/cabecalho", "rodapeStyle.css"); 
+    await carregarHTML("footer", "/rodape", "cabecalhoStyle.css");
+
+
+    
+    const iconButton = document.getElementById("iconButton");
+
+
+    if (iconButton) {
+        iconButton.addEventListener("click", function () {
+            window.location.href = "http://localhost:8080/login";
+        });
+    } else {
+        console.warn("Botão com id 'iconButton' não encontrado.");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", iniciar);
