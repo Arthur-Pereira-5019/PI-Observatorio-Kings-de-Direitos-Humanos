30c5d980533f234a148306f7ea81400df4a9dd3b
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-09-03 11:59:58 -0300

    Mudanças no Exception Handler Customizado

--- src/main/java/com/kings/okdhvi/exception/ExceptionHandlerCustomizado.java
+++ src/main/java/com/kings/okdhvi/exception/ExceptionHandlerCustomizado.java
@@ -12,12 +12,46 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Date;
 
 @RestController
 @ControllerAdvice
 public class ExceptionHandlerCustomizado extends ResponseEntityExceptionHandler {
+
+    /*
+    package com.kings.okdhvi.exception;
+
+import com.kings.okdhvi.exception.imagens.InvalidBase64ImageEncoding;
+import com.kings.okdhvi.exception.login.InvalidLoginInfoException;
+import com.kings.okdhvi.exception.postagem.RevisaoPostagemException;
+import com.kings.okdhvi.exception.usuario.*;
+import com.kings.okdhvi.exception.usuario.NullResourceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.Date;
+
+@Controller
+@ControllerAdvice
+public class StaticExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public final String handleAboutBlankException(Exception ex, WebRequest request) {
+        if(ex.getMessage().contains("No static resource")) {
+            return "telaInexistente";
+        }
+        return "telaInexistente";
+    }
+}
+     */
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
@@ -36,7 +70,6 @@ public class ExceptionHandlerCustomizado extends ResponseEntityExceptionHandler 
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
-
 
     @ExceptionHandler(RevisaoPostagemException.class)
     public final ResponseEntity<ExceptionResponse> handlePostagemJaRevisada(Exception ex, WebRequest request) {
