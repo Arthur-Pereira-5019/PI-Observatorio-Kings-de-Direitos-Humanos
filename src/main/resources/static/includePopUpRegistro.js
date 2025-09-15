f48ced19cf08b236391c0b94ca6a67edaccbf650
Daniel Stamm Pessoa de Oliveira <dani.stamm.pessoa.oliveira@gmail.com>
2025-09-15 12:07:21 -0300

    "popUp arrumado e operante"

--- src/main/resources/static/includePopUpRegistro.js
+++ src/main/resources/static/includePopUpRegistro.js
@@ -40,6 +40,57 @@ async function iniciar() {
             fundoPopup.style.display = "none";
         }
     });
+
+    const registerButton = document.getElementById("registerButton");
+
+const senhaInputRegistro = document.getElementById("senhaInputRegistro");
+const confSenhaInputRegistro = document.getElementById("confSenhaInputRegistro");
+const inputNomeRegistro = document.getElementById("inputNomeRegistro");
+const inputTelefoneRegistro = document.getElementById("inputTelefoneRegistro");
+const inputCpfRegistro = document.getElementById("inputCpfRegistro");
+const inputEmailRegistro = document.getElementById("inputEmailRegistro");
+const inputDataNascRegistro = document.getElementById("inputDataNascRegistro");
+
+registerButton.addEventListener("click", () => {
+  const novoPost = {
+    title: 'dadosRegistro',
+    nome: inputNomeRegistro.value,
+    senha: senhaInputRegistro.value,
+    telefone: inputTelefoneRegistro.value,
+    cpf: inputCpfRegistro.value,
+    email: inputEmailRegistro.value,
+    dataDeNascimento: inputDataNascRegistro.value
+  };
+
+console.log("oi")
+
+  fetch("http://localhost:8080/api/user", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(novoPost)
+  })
+  .then(res => {
+    if (!res.ok) throw new Error("Erro no servidor");
+    return res.json();
+  })
+  .then(data => {
+    console.log("Registro feito:", data);
+    inputNomeRegistro.value = '';
+    senhaInputRegistro.value = '';
+    confSenhaInputRegistro.value = '';
+    inputTelefoneRegistro.value = '';
+    inputCpfRegistro.value = '';
+    inputEmailRegistro.value = '';
+    inputDataNascRegistro.value = '';
+
+    window.location.href = "http://localhost:8080/"
+
+  })
+  .catch(err => console.error(err));
+});
+
 }
 
+
 document.addEventListener("DOMContentLoaded", iniciar);
+
