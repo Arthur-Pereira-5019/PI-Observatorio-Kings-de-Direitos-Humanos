93fa15a42d84a467b0e2fb43ef4f164e56fcbb29
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-09-23 18:19:36 -0300

    99% do popup Nova Imagem

--- src/main/java/com/kings/okdhvi/model/requests/CriarImagemRequest.java
+++ src/main/java/com/kings/okdhvi/model/requests/CriarImagemRequest.java
@@ -2,4 +2,4 @@ package com.kings.okdhvi.model.requests;
 
 import com.kings.okdhvi.model.Usuario;
 
-public record CriarImagemRequest (String imageBase64, Long usuarioId, String descricao, String titulo) {}
+public record CriarImagemRequest (String imageBase64, String descricao, String titulo) {}
