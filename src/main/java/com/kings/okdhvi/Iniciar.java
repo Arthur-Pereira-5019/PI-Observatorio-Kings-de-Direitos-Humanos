400ed0d8b156f7722051da4b8558cc786827879d
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-11-10 08:03:53 -0300

    Corrigidas mudanças pós import

--- src/main/java/com/kings/okdhvi/Iniciar.java
+++ src/main/java/com/kings/okdhvi/Iniciar.java
@@ -1,6 +1,5 @@
 package com.kings.okdhvi;
 
-import com.kings.okdhvi.model.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
