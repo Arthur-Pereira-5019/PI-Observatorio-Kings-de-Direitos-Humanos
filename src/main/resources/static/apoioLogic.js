7b54b87a822756559a3c1f033bdbaa48329d58aa
Daniel Stamm Pessoa de Oliveira <dani.stamm.pessao.oliveira@gmail.com>
2025-11-19 04:29:10 -0300

    its over

--- src/main/resources/static/apoioLogic.js
+++ src/main/resources/static/apoioLogic.js
@@ -2,24 +2,22 @@ const editApoioD = document.getElementById("editar-acolhimento-direita")
 const editApoioE = document.getElementById("editar-acolhimento-esquerda")
 const addApoio = document.getElementById("add-acolhimento")
 
-
-
 fetch("http://localhost:8080/api/user", {
-        headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json' },
+})
+    .then(res => {
+        if (!res.ok) throw new Error("Erro no servidor");
+        return res.json();
     })
-        .then(res => {
-            if (!res.ok) throw new Error("Erro no servidor");
-            return res.json();
-        })
-        .then(data => {
-            if (data.estadoDaConta == "ESPECIALISTA" || data.estadoDaConta == "ADMINISTRADOR") {
-                editApoioD.style.display = "flex"
-                editApoioE.style.display = "flex"
-                addApoio.style.display = "flex"
-                addApoio.addEventListener("click", function () {
-                    window.location.pathname = "novo_apoio";
-                })
-            }
+    .then(data => {
+        if (data.estadoDaConta == "ESPECIALISTA" || data.estadoDaConta == "ADMINISTRADOR") {
+            editApoioD.style.display = "flex"
+            editApoioE.style.display = "flex"
+            addApoio.style.display = "flex"
+            addApoio.addEventListener("click", function () {
+               
+            })
+        }
 
-        })
-        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
