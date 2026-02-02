01c9997a22a2f15c6cc2da9c1c47f7bd269da963
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2026-02-02 19:40:25 -0300

    Criado helper para dar fetch nas imagens. Capa de publicações sendo consumida e apresentada corretamente.

---
+++ src/main/resources/static/helper.js
@@ -0,0 +1,25 @@
+
+function fetchImagem(id,componente) {
+    let descricao
+    fetch("http://localhost:8080/api/imagem/" + id, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(res => {
+            if (!res.ok) throw new Error("Erro no servidor");
+            descricao = res.headers.get("descricao")
+            return res.blob();
+        })
+        .then(data => {
+            const url = URL.createObjectURL(data)
+            componente.src = url;
+            componente.title = descricao
+            componente.alt = descricao
+            componente.onload = () => {
+                URL.revokeObjectURL(url);
+            };
+            return true;
+        })
+        .catch(err => console.error(err));
+    return false;
+}
